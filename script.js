8bceb8c2df39f89315ce0c70776a524f9b3f2504
RenatoGomesdaPaz01 <166641094+RenatoGomesdaPaz01@users.noreply.github.com>
2024-04-10 10:44:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/* JavaScript Starter Code */
+let numbers = document.getElementById('initialNumbers');
+    // prompt("How many numbers?") 
+let result = document.getElementById('permutedNumbers');
+    
+let btn = document.getElementById('btnPermute');
+let resultDiv = document.getElementById('resultDiv');
+let numArray = [];
+
+function randomPermuteMe(numArray) {
+  // ToDo
+  // Math.random();
+
+}
+window.onload = function() {
+  // ToDo
+    var value = prompt("How many numbers?")
+    document.getElementById("initialNumbers").innerHTML = value;
+}
+btn.onclick = function() {
+  // ToDo
+
+}
